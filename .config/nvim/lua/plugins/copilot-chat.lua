50af94e4d199b9edbca1d6d8007dfdbeac6241e4
lys <yishu_li@brown.edu>
2025-10-02 15:52:53 -0400

    Set the default chat model to gpt 5

---
+++ .config/nvim/lua/plugins/copilot-chat.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    "CopilotC-Nvim/CopilotChat.nvim",
+    dependencies = {
+      { "nvim-lua/plenary.nvim", branch = "master" },
+    },
+    build = "make tiktoken",
+    opts = {
+      model = "gpt-5",
+      insert_at_end = true,
+    },
+  },
+}
--- .config/nvim/lua/plugins/mini-bufremove.lua
+++ .config/nvim/lua/plugins/mini-bufremove.lua
@@ -1,4 +1,4 @@
 return {
-  "echasnovski/mini.bufremove",
+  "nvim-mini/mini.bufremove",
   version = "*", -- 使用最新版本
 }
